a8b2ccc2ff87848c4fc98e509d7515b3db0b7235
TristanLemire <tristan.lemire.sio@gmail.com>
2020-03-16 12:30:13 +0100

    add axios

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 // Environement variable
 require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
+  });
+
+  app.post('/party', function(req, res) {
+    console.log(req.body)
+    axios
+    .post(`${process.env.API_URL}/party`, req.body)
+    .then(({data}) => console.log(data))
+    .catch((err) => console.error(err));  
   });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
