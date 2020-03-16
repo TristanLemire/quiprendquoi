ed639ec861d0e9194da7bdcd0bf3bc70fd79bbbc
TristanLemire <tristan.lemire.sio@gmail.com>
2020-03-16 11:59:22 +0100

    add template variable title

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', { title: 'Qui prend quoi ?' });
   });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
