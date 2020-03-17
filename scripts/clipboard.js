e5bb9aa560ef7edd560165245b9f3fc30e9d7206
TristanLemire <tristan.lemire.sio@gmail.com>
2020-03-17 11:57:25 +0100

    end cours 2

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -1,23 +1,23 @@
 if (navigator.clipboard) {
-    document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
-        const $button = document.createElement('button');
-        $button.innerHTML = 'Copier';
-        $button.addEventListener(
-            'click',
-            copyToClipboard.bind(this, $clipboardEl, $button)
-          );
-        $clipboardEl.parentNode.append($button);
-      });
-  } else {
-    console.warn("Pas de support :(")
-  }
-  
-  function copyToClipboard($clipboardEl, $button) {
-    navigator.clipboard
-  .writeText($clipboardEl.getAttribute('data-clipboard'))
-  .then(() => {
-    $button.innerHTML = 'Copié !';
-    setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
-  })
-  .catch((err) => console.warn(err));
-  }
+  document.querySelectorAll("[data-clipboard]").forEach($clipboardEl => {
+    const $button = document.createElement("button");
+    $button.innerHTML = "Copier";
+    $button.addEventListener(
+      "click",
+      copyToClipboard.bind(this, $clipboardEl, $button)
+    );
+    $clipboardEl.parentNode.append($button);
+  });
+} else {
+  console.warn("Pas de support :(");
+}
+
+function copyToClipboard($clipboardEl, $button) {
+  navigator.clipboard
+    .writeText($clipboardEl.getAttribute("data-clipboard"))
+    .then(() => {
+      $button.innerHTML = "Copié !";
+      setTimeout(() => ($button.innerHTML = "Copier"), 2000);
+    })
+    .catch(err => console.warn(err));
+}
---
+++ scripts/findMe.js
@@ -0,0 +1,31 @@
+function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.google.fr/maps/@${latitude}/${longitude}`;
+      mapLink.textContent = `voir sur google map`;
+    }
+  
+    function error() {
+      status.textContent = "Impossible d'avoir votre geolocalisation";
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation est pas supporter par votre navigateur';
+    } else {
+      status.textContent = 'Chargement…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
